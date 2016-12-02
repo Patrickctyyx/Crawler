a79883e3b623f417225a474bacf5cca4dbff27ef
Patrickctyyx <873948000@qq.com>
2016-12-03 00:12:20 +0800

    12.2 Daily Learn:     Another attempt to login the educational administration system of JNU.     finished!

--- JNUjwc.py
+++ JNUjwc.py
@@ -13,25 +13,37 @@ headers = {
 url = 'http://jwxt.jnu.edu.cn/'
 req = session.get(url, headers=headers)
 bsObj = BeautifulSoup(req.text, 'lxml')
-captchaUrl = url + bsObj.findAll('img', {'alt': '看不清再换一张'})[0].attrs['src']
-# urlretrieve(captchaUrl, '/home/patrick/音乐/Crawler/capt')
-
-hiddens = bsObj.findAll('input', {'type': 'hidden'})
-print(hiddens)
-values = []
-for hidden in hiddens:
-    values.append((hidden.attrs['name'], hidden.attrs['value']))
-print(values)
 
 
-passwd = input(u'请手动输入密码\n>')
-captcha = input(u'请手动输入验证码\n>')
-postdata = urlencode({
-    values[0][0]: values[0][1],
-    values[1][0]: values[1][1],
-    values[2][0]: values[2][1],
-    'txtYHBS': '2015053961',
-    'txtYHMM': passwd,
-    'txtFJM': captcha,
-    'btnLogin': '%B5%C7++++%C2%BC'
-})
+def save_captach():
+    captchaUrl = url + bsObj.findAll('img', {'alt': '看不清再换一张'})[0].attrs['src']
+    urlretrieve(captchaUrl, '/home/patrick/音乐/Crawler/capt')
+
+
+def login():
+    hiddens = bsObj.findAll('input', {'type': 'hidden'})
+    values = []
+    for hidden in hiddens:
+        values.append((hidden.attrs['name'], hidden.attrs['value']))
+
+    passwd = input(u'请手动输入密码\n>')
+    captcha = input(u'请手动输入验证码\n>')
+
+    postdata = urlencode({
+        values[0][0]: values[0][1],
+        values[1][0]: values[1][1],
+        values[2][0]: values[2][1],
+        'txtYHBS': '2015053961',
+        'txtYHMM': passwd,
+        'txtFJM': captcha,
+        'btnLogin': '%B5%C7++++%C2%BC'
+    })
+    # login
+    session.post(url + 'Login.aspx', data=postdata, headers=headers)
+
+    succ = session.get('http://jwxt.jnu.edu.cn/IndexPage.aspx')
+    print('登陆成功！')
+
+if __name__ == '__main__':
+    save_captach()
+    login()
